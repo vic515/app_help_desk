a09c222277e633d952857b77e43dfd3e945b9aaf
vic515 <63172583+vic515@users.noreply.github.com>
2020-09-04 17:49:19 -0300

    Começando autenticação

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -39,6 +41,15 @@
                 <div class="form-group">
                   <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+                <?php
+
+                  if(isset($_GET['login']) && $_GET['login'] == 'erro') {
+                    ?>
+                  <div class="text-danger">
+                    Usuário ou senha inválidos
+                  </div>
+                <?php } ?>
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
--- valida-login.php
+++ valida-login.php
@@ -1,3 +1,15 @@
 <?php
-    print_r($_POST);
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+        array('email' => 'lemosvict@gmail.com', 'senha' => 'papeldecarta')
+    );
+    echo '<pre>';
+    print_r($usuarios_app);
+    echo '</pre>';
+    if(in_array($_POST,$usuarios_app)) {
+        echo "<hr><hr> Usuário autenticado com sucesso";
+    } else {
+        header('Location: index.php?login=erro');
+    }
  ?>
